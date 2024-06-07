1231975525467761ec65236de0b52ac7a8bfd1ea
Harsh Virdi <gursharansingh@Harshs-MacBook-Pro.local>
2024-06-07 12:51:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function ImageGallery() {
+  return <div>Image Gallery</div>;
+}
+
+function QuickNavMenu() {
+  return <div>Quick Navigation Menu</div>;
+}
+
+function StandardMenu() {
+  return <div>Standard Menu</div>;
+}
+
+function Content() {
+  return <div>Content Section</div>;
+}
+
 function App() {
+  const [layout, setLayout] = useState({
+    prefersVisualContent: true,
+    prefersQuickNavigation: false
+  });
+
+  const toggleVisualContent = () => {
+    setLayout(prevLayout => ({
+      ...prevLayout,
+      prefersVisualContent: !prevLayout.prefersVisualContent
+    }));
+  };
+
+  const toggleQuickNavigation = () => {
+    setLayout(prevLayout => ({
+      ...prevLayout,
+      prefersQuickNavigation: !prevLayout.prefersQuickNavigation
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={toggleVisualContent}>Toggle Visual Content</button>
+      <button onClick={toggleQuickNavigation}>Toggle Quick Navigation</button>
+      {layout.prefersVisualContent && <ImageGallery />}
+      {layout.prefersQuickNavigation && <QuickNavMenu />}
+      {!layout.prefersQuickNavigation && <StandardMenu />}
+      <Content />
     </div>
   );
 }
 
 export default App;
+
